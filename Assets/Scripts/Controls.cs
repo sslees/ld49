1644dcdf89f968d61feb343a0cde5c206301809d
sslees <sam@sslees.com>
2021-10-03 15:53:49 -0600

    submission

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Controls : MonoBehaviour
 {
     public GameObject verticalBeam;
     public GameObject horizontalPlatform;
+
+    bool reset = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,11 @@ public class Controls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        if (reset)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
         {
             var mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
             var world = Camera.main.ScreenToWorldPoint(mouse);
@@ -30,5 +37,9 @@ public class Controls : MonoBehaviour
                 Instantiate(horizontalPlatform, world, Quaternion.identity);
             }
         }
+    }
+    private void OnMouseDown()
+    {
+        reset = true;
     }
 }
